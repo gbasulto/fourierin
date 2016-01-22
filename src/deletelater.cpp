93fe20bad77766b11a04fca0d2946bddd3d7447f
Guillermo Basulto-Elias <guillermobasulto@gmail.com>
2016-01-22 17:41:22 -0600

    A template for a package

---
+++ src/deletelater.cpp
@@ -0,0 +1,22 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//
+
+//' Times two
+//'
+//' It mulpliplies a vector by two.
+//' @export
+// [[Rcpp::export]]
+NumericVector timesTwo(NumericVector x) {
+  return x * 2;
+}
+
