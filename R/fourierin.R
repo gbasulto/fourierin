17ea63451a7e426046437e7283b6d9186bab37de
Guillermo Basulto-Elias <guillermobasulto@gmail.com>
2016-01-24 18:54:33 -0600

    Start writing wrapper function fourierin.

---
+++ R/fourierin.R
@@ -0,0 +1,14 @@
+#' Compute Fourier integrals
+#'
+#' It computes Fourier integrals for functions of one and two
+#' variables.
+#' @export
+fourierin <- function(f, a, b, c, d, r, s){
+    
+    NULL
+}
+
+#' @useDynLib fourierin
+#' @importFrom Rcpp sourceCpp
+NULL
+
