cc975eb142210481fb63b2f442024f540eeb464c
Guillermo Basulto-Elias <guillermobasulto@gmail.com>
2016-02-03 21:08:21 -0600

    Add 1d wrapper including its documentation.

--- R/fourierin.R
+++ R/fourierin.R
@@ -1,3 +1,55 @@
+#' Compute Univariate Fourier integrals
+#'
+#' It computes Fourier integrals for functions of one and two
+#' variables.
+#'
+#' @param f A function which can be evaluated in matrices of n columns
+#'     (n = 1 or n = 2). Or a matrix of n columns with f already
+#'     evaluated.
+#' @param a Lower integration limit(s).
+#' @param b Upper integration limit(s).
+#' @param c Lower evaluation limit(s).
+#' @param d Upper evaluation limit(s).
+#' @param r Factor related to adjust definition of Fourier
+#'     transform. It is usually 0, -1 or 1.
+#' @param s Constant to adjust the exponent on the definition of the
+#'     Fourier transform. It is usually 1, -1, 2pi or -2pi.
+#'
+#' @return A list with the elements
+#' \item{w}{ddd}
+#' \item{values}{dddd}
+#'
+#' @example
+#' examples/ex_fourierin_1d.R
+#' @export
+fourierin_1d <- function(f, a, b, c, d, r, s, resol = NULL){
+    ## If function values are provided, then the resolution
+    ## is the length of the vector of values.
+    if(!is.function(f)) resol <- length(f)
+
+    ## Increment in the frequency domain.
+    gam <- (d - c)/resol
+
+    ## Freq. dom. vector.
+    w <- seq(c, d - gam, length.out = resol)
+
+    ## If f is the function, it needs to be evaluated in
+    ## the time domain values.
+    if(is.function(f)){
+        del <- (b - a)/resol # Increment in the time
+                                        # domain.
+        t <- seq(a + del/2, b - del/2,
+                 length.out = resol)    # Freq. dom. vector.
+        out <- fourierin_1d_cpp(f(t), a, b, c, d, r, s)
+    } else{
+        out <- fourierin_1d_cpp(f, a, b, c, d, r, s)
+    }
+
+    return(list(w = w,                  # Return list.
+                values = out))
+}
+
+
 #' Compute Fourier integrals
 #'
 #' It computes Fourier integrals for functions of one and two
@@ -30,27 +82,7 @@ fourierin <- function(f, a, b, c, d, r, s, resol = NULL){
     switch(n,
            ## --- Case n = 1 -----------------------------------------
            "1" = {
-               ## If function values are provided, then the resolution
-               ## is the length of the vector of values.
-               if(!is.function(f)) resol <- length(f)
-
-               ## Increment in the frequency domain.
-               gam <- (d - c)/resol
-
-               ## Freq. dom. vector.
-               w <- seq(c, d - gam, length.out = resol)
-
-               ## If f is the function, it needs to be evaluated in
-               ## the time domain values.
-               if(is.function(f)){
-                   del <- (b - a)/resol # Increment in the time
-                                        # domain.
-                   t <- seq(a + del/2, b - del/2,
-                            length.out = resol)    # Freq. dom. vector.
-                   out <- fourierin_1d_cpp(f(t), a, b, c, d, r, s)
-               } else{
-                   out <- fourierin_1d_cpp(f, a, b, c, d, r, s)
-               }
+               return(fourierin_1d(f, a, b, c, d, r, s, resol))
            },
            ## --- Case n = 2 -----------------------------------------
            "2" = {
@@ -77,12 +109,10 @@ fourierin <- function(f, a, b, c, d, r, s, resol = NULL){
                   out <- fourierin_2d_cpp(f(t), a, b, c, d, r, s)
                } else{
                    out <- fourierin_2d_cpp(f, a, b, c, d, r, s)
+                   return(out)
                }
            } # End n =2
            ) # End switch
-
-    return(list(w = w,                  # Return list.
-                values = out))
 }
 
 #' @useDynLib fourierin
