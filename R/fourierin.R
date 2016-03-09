1cb4d0b4fab78095e717d4e0338fe5259f1087e1
Guillermo Basulto-Elias <basulto@iastate.edu>
2016-03-09 15:48:33 -0600

    Adapt wrapper function to work with complex valued functions.

--- R/fourierin.R
+++ R/fourierin.R
@@ -27,34 +27,42 @@
 #' examples/ex_fourierin_1d.R
 #' @export
 fourierin_1d <- function(f, a, b, c, d, r, s, resol = NULL){
-    ## If function values are provided, then the resolution
-    ## is the length of the vector of values.
-    if(!is.function(f)) resol <- length(f)
+  ## If function values are provided, then the resolution
+  ## is the length of the vector of values.
+  if(!is.function(f)) resol <- length(f)
 
-    ## Increment in the frequency domain.
-    gam <- (d - c)/resol
+  ## Increment in the frequency domain.
+  gam <- (d - c)/resol
 
-    ## Freq. dom. vector.
-    w <- seq(c, d - gam, length.out = resol)
+  ## Freq. dom. vector.
+  w <- seq(c, d - gam, length.out = resol)
 
-    ## If f is the function, it needs to be evaluated in
-    ## the time domain values.
-    if(is.function(f)){
-        del <- (b - a)/resol # Increment in the time
-                                        # domain.
-        t <- seq(a + del/2, b - del/2,
-                 length.out = resol)    # Freq. dom. vector.
-        f_t <- f(t)                     # Function values
-              # Rutinary check
-        if(is.null(f_t)) stop("Function f is null.")
+  ## If f is the function, it needs to be evaluated in
+  ## the time domain values.
+  if(is.function(f)){
+    del <- (b - a)/resol # Increment in the time
+    # domain.
+    t <- seq(a + del/2, b - del/2,
+             length.out = resol)    # Freq. dom. vector.
+    f_t <- f(t)                     # Function values
+    # Rutinary check
+    if(is.null(f_t)) stop("Function f is null.")
 
-        out <- fourierin_1d_cpp(f_t, a, b, c, d, r, s)
+    if(is.complex(f_t)){
+      out <- fourierin_cx_1d_cpp(f_t, a, b, c, d, r, s)
     } else{
-        out <- fourierin_1d_cpp(f, a, b, c, d, r, s)
+      out <- fourierin_1d_cpp(f_t, a, b, c, d, r, s)
     }
+  } else{
+    if(is.complex(f)){
+      out <- fourierin_cx_1d_cpp(f, a, b, c, d, r, s)
+    } else{
+      out <- fourierin_1d_cpp(f, a, b, c, d, r, s)
+    }
+  }
 
-    return(list(w = w,                  # Return list.
-                values = out))
+  return(list(w = w,                  # Return list.
+              values = out))
 }
 
 #' Bivariate Fourier integrals
@@ -90,41 +98,40 @@ fourierin_1d <- function(f, a, b, c, d, r, s, resol = NULL){
 #' examples/ex_fourierin_2d.R
 #' @export
 fourierin_2d <- function(f, a, b, c, d, r, s, resol = NULL){
-    ## If function values are provided, then the resolution is the
-    ## length of the vector of values.
-    if(!is.function(f)) resol <- dim
+  ## If function values are provided, then the resolution is the
+  ## length of the vector of values.
+  if(!is.function(f)) resol <- dim
 
-    ## Increment in the frequency domain.
-    gam <- (d - c)/resol
+  ## Increment in the frequency domain.
+  gam <- (d - c)/resol
 
-    ## Freq. dom. vectors.
-    w1 <- seq(c[1], d[1] - gam[1], length.out = resol[1])
-    w2 <- seq(c[2], d[2] - gam[2], length.out = resol[2])
+  ## Freq. dom. vectors.
+  w1 <- seq(c[1], d[1] - gam[1], length.out = resol[1])
+  w2 <- seq(c[2], d[2] - gam[2], length.out = resol[2])
 
-    ## If f is the function, it needs to be evaluated in the time
-    ## domain values.
-    if(is.function(f)){
-        del <- (b - a)/resol # Increment in the time
-                                        # domain.
-        t1 <- seq(a[1] + del[1]/2, b[1] - del[1]/2,
-                  length.out = resol[1]) # Freq. dom. vector.
-        t2 <- seq(a[2] + del[2]/2, b[2] - del[2]/2,
-                  length.out = resol[1]) # Freq. dom. vector.
-        t <- as.matrix(expand.grid(t1, t2))
-        f_vals <- matrix(f(t), resol[1], resol[2])
+  ## If f is the function, it needs to be evaluated in the time
+  ## domain values.
+  if(is.function(f)){
+    del <- (b - a)/resol # Increment in the time
+    # domain.
+    t1 <- seq(a[1] + del[1]/2, b[1] - del[1]/2,
+              length.out = resol[1]) # Freq. dom. vector.
+    t2 <- seq(a[2] + del[2]/2, b[2] - del[2]/2,
+              length.out = resol[1]) # Freq. dom. vector.
+    t <- as.matrix(expand.grid(t1, t2))
+    f_vals <- matrix(f(t), resol[1], resol[2])
 
-        # Rutinary check
-        if(is.null(f_vals)) stop("Function f is null.")
+    # Rutinary check
+    if(is.null(f_vals)) stop("Function f is null.")
 
-        out <- fourierin_2d_cpp(f_vals, a, b, c, d, r, s)
-#         out <- fourierin_2d_cpp(outer(t1, t2, f), a, b, c, d, r, s)
-    } else{
-        out <- fourierin_2d_cpp(f, a, b, c, d, r, s)
-    }
+    out <- fourierin_2d_cpp(f_vals, a, b, c, d, r, s)
+  } else{
+    out <- fourierin_2d_cpp(f, a, b, c, d, r, s)
+  }
 
-    return(list(w1 = w1,
-                w2 = w2,
-                values = out))
+  return(list(w1 = w1,
+              w2 = w2,
+              values = out))
 }
 
 
@@ -161,13 +168,13 @@ fourierin_2d <- function(f, a, b, c, d, r, s, resol = NULL){
 #' @export
 fourierin <- function(f, a, b, c, d, r, s, resol = NULL){
 
-    n <- length(a)                      # Get dimension of function
-                                        # from lower integration
-                                        # limit.
-    switch(n,
-           return(fourierin_1d(f, a, b, c, d, r, s, resol)),
-           return(fourierin_2d(f, a, b, c, d, r, s, resol))
-           ) # End switch
+  n <- length(a)                      # Get dimension of function
+  # from lower integration
+  # limit.
+  switch(n,
+         return(fourierin_1d(f, a, b, c, d, r, s, resol)),
+         return(fourierin_2d(f, a, b, c, d, r, s, resol))
+  ) # End switch
 }
 
 #' @useDynLib fourierin
