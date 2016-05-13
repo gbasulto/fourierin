d8336d3d07488a582c5606e711c497bbb329c83a
Guillermo Basulto-Elias <basulto@iastate.edu>
2016-05-13 12:39:23 -0500

    Comment on fourierin output.

--- R/fourierin.R
+++ R/fourierin.R
@@ -158,6 +158,7 @@ fourierin_2d <- function(f, a, b, c, d, r, s, resol = NULL,
     }
 
 
+    ## Return grid if it was not provided.
     if (is.null(w)) {
       return(list(w1 = w1,
                   w2 = w2,
