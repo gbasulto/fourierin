aee4bffc8ff7c9f47f7f576926f35e6906fab518
Guillermo Basulto-Elias <guillermobasulto@gmail.com>
2019-05-15 16:06:22 -0500

    Add 1D tests.

--- tests/testthat/test-fourierin_1d.R
+++ tests/testthat/test-fourierin_1d.R
@@ -12,7 +12,8 @@ test_that("fourierin works for 1d", {
 })
 
 
-test_that("error if evaluation grid nor evaluation bounds are provided", {
+test_that(
+  "error if evaluation grid nor evaluation bounds are provided", {
   ## Function to be used in the integrand
   myfnc <- function(t) exp(-t^2/2)
 
@@ -22,3 +23,26 @@ test_that("error if evaluation grid nor evaluation bounds are provided", {
               const_adj = -1,
               freq_adj = -1, resolution = 64))
 })
+
+
+test_that(
+  "evaluate on a given set of points (real function)", {
+    myfnc <- function(t) exp(-t^2/2)
+    out <- fourierin(f = myfnc, lower_int = -5, upper_int = 5,
+                     const_adj = -1, freq_adj = -1, resolution = 64, 
+                     eval_grid = 0:1)
+    expect_type(out, "complex")
+  }
+)
+
+test_that(
+  "evaluate on a given set of points (complex function)", {
+    myfnc <- function(t) exp(-1i*t^2/2)
+    out <- fourierin(f = myfnc, lower_int = -5, upper_int = 5,
+                     const_adj = -1, freq_adj = -1, resolution = 64, 
+                     eval_grid = 0:1)
+    expect_type(out, "complex")
+  }
+)
+
+
