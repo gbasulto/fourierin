a2c7a7d54ca95589e5520f01179918cd3534e211
Guillermo Basulto-Elias <guillermobasulto@gmail.com>
2019-05-17 23:54:59 -0500

    Add test for univ. complex function.

--- tests/testthat/test-fourierin_1d.R
+++ tests/testthat/test-fourierin_1d.R
@@ -1,9 +1,20 @@
 context("univariate")
 
-test_that("fourierin works for 1d", {
+test_that("fourierin works for 1d for real function", {
   ## Function to be used in the integrand
   myfnc <- function(t) exp(-t^2/2)
 
+  ## Compute integral
+  out <- fourierin(f = myfnc, lower_int = -5, upper_int = 5,
+                   lower_eval= -3, upper_eval = 3, const_adj = -1,
+                   freq_adj = -1, resolution = 64)
+  expect_is(out, "list")
+})
+
+test_that("fourierin works for 1d for complex function", {
+  ## Function to be used in the integrand
+  myfnc <- function(t) exp(-1i*t^2/2)
+  
   ## Compute integral
   out <- fourierin(f = myfnc, lower_int = -5, upper_int = 5,
                    lower_eval= -3, upper_eval = 3, const_adj = -1,
