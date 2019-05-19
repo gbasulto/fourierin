ee9caa47e1ecfadfa6ff929960ca0f25f52ed580
Guillermo Basulto <guillermobasulto@gmail.com>
2019-05-18 20:33:12 -0500

    Add test evaluating f outside function.

--- tests/testthat/test-fourierin_1d.R
+++ tests/testthat/test-fourierin_1d.R
@@ -14,7 +14,7 @@ test_that("fourierin works for 1d for real function", {
 test_that("fourierin works for 1d for complex function", {
   ## Function to be used in the integrand
   myfnc <- function(t) exp(-1i*t^2/2)
-  
+
   ## Compute integral
   out <- fourierin(f = myfnc, lower_int = -5, upper_int = 5,
                    lower_eval= -3, upper_eval = 3, const_adj = -1,
@@ -40,7 +40,7 @@ test_that(
   "evaluate on a given set of points (real function)", {
     myfnc <- function(t) exp(-t^2/2)
     out <- fourierin(f = myfnc, lower_int = -5, upper_int = 5,
-                     const_adj = -1, freq_adj = -1, resolution = 64, 
+                     const_adj = -1, freq_adj = -1, resolution = 64,
                      eval_grid = 0:1)
     expect_type(out, "complex")
   }
@@ -50,10 +50,30 @@ test_that(
   "evaluate on a given set of points (complex function)", {
     myfnc <- function(t) exp(-1i*t^2/2)
     out <- fourierin(f = myfnc, lower_int = -5, upper_int = 5,
-                     const_adj = -1, freq_adj = -1, resolution = 64, 
+                     const_adj = -1, freq_adj = -1, resolution = 64,
                      eval_grid = 0:1)
     expect_type(out, "complex")
   }
 )
+
+
+test_that(
+  "evaluate f outside fourierin", {
+    t <- seq(-5, 5 - 10/64, length.out = 64) + 10/64/2
+    f <- exp(-t^2/2)
+    out <- fourierin(f = f, lower_int = -5, upper_int = 5,
+                     const_adj = -1, freq_adj = -1,
+                     eval_grid = 0:1)
+
+    expect_type(out, "complex")
+  }
+)
+
+
+
+
+
+
+
 
 
